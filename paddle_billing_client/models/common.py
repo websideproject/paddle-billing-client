784740efc49b65c46c5ad4cf8d4c170e63c15c8a
bgervan <bgervan@users.noreply.github.com>
2023-09-06 01:41:09 +0000

    Update API Reference

--- paddle_billing_client/models/common.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class BillingPeriod(BaseModel):
-    ends_at: str
-    starts_at: str
--- tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-@pytest.fixture(autouse=True)
-def vcr_config():
-    return {"decode_compressed_response": True, "record_mode": "once"}
