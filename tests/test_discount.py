e3159691b99d37eb0e83fc404732d17651989d49
Benjamin Gervan <gben9302@gmail.com>
2025-03-30 19:23:35 +0200

    Update dependencies

--- tests/test_discount.py
+++ tests/test_discount.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 import deepdiff
 import pytest
@@ -38,7 +38,7 @@ class TestDiscount:
                 maximum_recurring_intervals=12,
                 usage_limit=100,
                 restrict_to=["pro_01h89b2j66qq82x6vn5d39c4av"],
-                expires_at=(datetime.utcnow() + timedelta(days=30)).isoformat(),
+                expires_at=(datetime.now(timezone.utc) + timedelta(days=30)).isoformat(),
             ),
         )
         expected_discount = DiscountResponse(
